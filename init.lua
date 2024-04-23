1cbed62e5e69be9dabf5dd56c71f05ee3d52a520
Dirk Porsche <d.porsche@verband.creditreform.de>
2024-04-23 11:13:36 +0200

    something

--- init.lua
+++ init.lua
@@ -875,7 +875,10 @@ require("lazy").setup({
 			--    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
 		end,
 	},
-
+	{
+		"sudormrfbin/cheatsheet.nvim",
+		dependencies = { "nvim-lua/popup.nvim", "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
+	},
 	-- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
 	-- init.lua. If you want these files, they are in the repository, so you can just download them and
 	-- place them in the correct locations.
