d5611b0ade071e38f25bae10d70346249281c4dd
Dirk Porsche <dirk@accoma.de>
2024-08-15 07:45:15 +0200

    added binding

--- init.lua
+++ init.lua
@@ -126,6 +126,13 @@ vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagn
 vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 
+-- Open terminal at current files position
+vim.keymap.set("n", "<leader>tc", function()
+	vim.cmd("cd %:p:h")
+	vim.cmd("terminal")
+	vim.cmd("startinsert")
+end, { desc = "Open [t]erminal at [c]urrent dir" })
+
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
 -- is not what someone will guess without a bit more experience.
