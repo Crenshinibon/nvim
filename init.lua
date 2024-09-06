44ef5d9556b79aa5f69bef187904f0252ed1a97c
Dirk Porsche <dirk@accoma.de>
2024-09-06 13:54:57 +0200

    added nvim recorder v

--- init.lua
+++ init.lua
@@ -1,6 +1,7 @@
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+--
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
@@ -242,6 +243,62 @@ end
 -- NOTE: Here is where you install your plugins.
 require("lazy").setup({
 	-- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
+	{
+		"chrisgrieser/nvim-recorder",
+		dependencies = "rcarriga/nvim-notify", -- optional
+		opts = {
+			-- Named registers where macros are saved (single lowercase letters only).
+			-- The first register is the default register used as macro-slot after
+			-- startup.
+			slots = { "a", "b" },
+
+			mapping = {
+				startStopRecording = "q",
+				playMacro = "Q",
+				switchSlot = "<C-q>",
+				editMacro = "cq",
+				deleteAllMacros = "dq",
+				yankMacro = "yq",
+				-- ⚠️ this should be a string you don't use in insert mode during a macro
+				addBreakPoint = "##",
+			},
+
+			-- Clears all macros-slots on startup.
+			clear = false,
+
+			-- Log level used for non-critical notifications; mostly relevant for nvim-notify.
+			-- (Note that by default, nvim-notify does not show the levels `trace` & `debug`.)
+			logLevel = vim.log.levels.INFO, -- :help vim.log.levels
+
+			-- If enabled, only essential notifications are sent.
+			-- If you do not use a plugin like nvim-notify, set this to `true`
+			-- to remove otherwise annoying messages.
+			lessNotifications = false,
+
+			-- Use nerdfont icons in the status bar components and keymap descriptions
+			useNerdfontIcons = true,
+
+			-- Performance optimzations for macros with high count. When `playMacro` is
+			-- triggered with a count higher than the threshold, nvim-recorder
+			-- temporarily changes changes some settings for the duration of the macro.
+			performanceOpts = {
+				countThreshold = 100,
+				lazyredraw = true, -- enable lazyredraw (see `:h lazyredraw`)
+				noSystemClipboard = true, -- remove `+`/`*` from clipboard option
+				autocmdEventsIgnore = { -- temporarily ignore these autocmd events
+					"TextChangedI",
+					"TextChanged",
+					"InsertLeave",
+					"InsertEnter",
+					"InsertCharPre",
+				},
+			},
+
+			-- [experimental] partially share keymaps with nvim-dap.
+			-- (See README for further explanations.)
+			dapSharedKeymaps = false,
+		}, -- required even with default settings, since it calls `setup()`
+	},
 	{
 		"kylechui/nvim-surround",
 		event = "VeryLazy",
@@ -732,7 +789,7 @@ require("lazy").setup({
 				vim.keymap.set("n", "O", api.node.open.no_window_picker, mapOpts("Open: No Window Picker", bufnr))
 				vim.keymap.set("n", "p", api.fs.paste, mapOpts("Paste", bufnr))
 				vim.keymap.set("n", "P", api.node.navigate.parent, mapOpts("Parent Directory", bufnr))
-				vim.keymap.set("n", "q", api.tree.close, mapOpts("Close", bufnr))
+				-- vim.keymap.set("n", "q", api.tree.close, mapOpts("Close", bufnr))
 				vim.keymap.set("n", "r", api.fs.rename, mapOpts("Rename", bufnr))
 				vim.keymap.set("n", "R", api.tree.reload, mapOpts("Refresh", bufnr))
 				vim.keymap.set("n", "s", api.node.run.system, mapOpts("Run System", bufnr))
